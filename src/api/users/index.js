f68015f54b41d6058ce607302f67c1f2e830c4a6
Courtney Hampton <jaye0888@gmail.com>
2023-03-02 13:16:27 +0100

    update to google redirect
    Realized that endpoint was structured incorrectly for OAuth.

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -38,7 +38,7 @@ usersRouter.get(
   passport.authenticate("google", { session: false }),
   async (req, res, next) => {
     console.log(req.user);
-    res.redirect(`${process.env.FE_URL}?accessToken=${req.user.accessToken}`);
+    res.redirect(`${process.env.FE_URL}/${req.user.accessToken}`);
   }
 );
 //logIn
