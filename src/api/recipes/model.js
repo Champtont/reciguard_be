15e008a0cc649c59cd5c75f9e5ec2c1e74fd6af9
Courtney Hampton <jaye0888@gmail.com>
2023-03-04 11:49:48 +0100

    Update to Recipe schema
    The schema was not expecting the correct type from the front end

--- src/api/recipes/model.js
+++ src/api/recipes/model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
 const recipeSchema = new Schema({
-  categoryTags: [{ type: String, required: true }],
+  categoryTags: [{ type: Object, required: true }],
   title: { type: String, required: true },
   photo: {
     type: String,
@@ -11,8 +11,8 @@ const recipeSchema = new Schema({
     default: "https://cdn-icons-png.flaticon.com/512/135/135161.png",
   },
   description: { type: String, required: false },
-  ingredients: [{ type: String, required: false }],
-  instructions: [{ type: String, required: false }],
+  ingredients: [{ type: Object, required: false }],
+  instructions: [{ type: Object, required: false }],
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
